9e85d104ded91a737828cff4ba88918a8f2e70fb
Chung Tran <bigearsenal@gmail.com>
2023-10-04 15:09:06 +0700

    fix: images

--- Scaffold/Scenes/Home/HomeView.swift
+++ Scaffold/Scenes/Home/HomeView.swift
@@ -9,6 +9,9 @@ public struct HomeView: View {
 
     public var body: some View {
         VStack {
+            Image(.flower)
+                .resizable()
+                .frame(width: 200, height: 200)
             Text("Hello!")
             Button(action: {
                 Task {
